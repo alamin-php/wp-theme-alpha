227992fceb49c1057013096d002762459788ac6b
alamin-php <alamin.php100@gmail.com>
2021-05-19 00:22:43 +0600

    updated blog posts

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function alpha_bootstrapping(){
 	load_theme_textdomain("alpha");
 	add_theme_support("title-tag");
+	add_theme_support( 'post-thumbnails', array( 'post' ) );
 }
 add_action("after_setup_theme", "alpha_bootstrapping");
 
