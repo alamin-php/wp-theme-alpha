66d0c82b0f90c744fea73ce3cfa0e35c98bef2dc
alamin <dev.alaminsarker@gmail.com>
2021-05-19 17:08:30 +0600

    added single post pagination

--- single.php
+++ single.php
@@ -8,24 +8,25 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
-                            <h2 class="post-title"><?php the_title(); ?></h2>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-4">
-                            <p>
+                            <h2 class="post-title text-center"><?php the_title(); ?></h2>
+                            <p class="text-center">
                                 <strong><?php the_author();?></strong><br/>
                                 <?php echo get_the_date("jS M, Y");?>
                             </p>
-                            <?php the_tags( '<ul class="list-unstyled"><li>', '</li><li>', '</li></ul>' ); ?>
-                        </div>
-                        <div class="col-md-8">
                             <p>
                                 <?php if(has_post_thumbnail()) : ?>
                                     <?php the_post_thumbnail('large', array('class' => 'img-fluid')); ?>
                                 <?php endif; ?>
                             </p>
                             <?php the_content();?>
+                            <div class="post-pag-wrap">
+                                <div class="post-pag-container prev">
+                                    <?php previous_post_link('<span>Previous</span><h3>%link</h3>', '%title', false);?>
+                                </div>                                
+                                <div class="post-pag-container next">
+                                    <?php next_post_link('<span>Next</span><h3>%link</h3>', '%title', false);?>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
