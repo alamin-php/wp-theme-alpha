c272f8ec59277e145fb0499a98dc172535b612f3
alamin <dev.alaminsarker@gmail.com>
2021-05-20 16:43:09 +0600

    added footer wigdet

--- functions.php
+++ functions.php
@@ -15,13 +15,31 @@ add_action("wp_enqueue_scripts", "alpha_assets");
 
 function alpha_sidebar_widget() {
     register_sidebar( array(
-        'name'          => __( 'Main Sidebar', 'textdomain' ),
+        'name'          => __( 'Main Sidebar', 'alpha' ),
         'id'            => 'sidebar-1',
-        'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'textdomain' ),
+        'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'alpha' ),
         'before_widget' => '<div id="%1$s" class="widget %2$s">',
         'after_widget'  => '</div>',
         'before_title'  => '<h2 class="widgettitle">',
         'after_title'   => '</h2>',
+    ) );    
+    register_sidebar( array(
+        'name'          => __( 'Footer Left', 'alpha' ),
+        'id'            => 'footer-left',
+        'description'   => __( 'Footer Left widget', 'alpha' ),
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '',
+        'after_title'   => '',
+    ) );    
+    register_sidebar( array(
+        'name'          => __( 'Footer Right', 'alpha' ),
+        'id'            => 'footer-right',
+        'description'   => __( 'Footer Right widget', 'alpha' ),
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '',
+        'after_title'   => '',
     ) );
 }
 add_action( 'widgets_init', 'alpha_sidebar_widget' );
